2db4c01b76efbfea101c0549815e309da3d1f88b
Алексей Адищев <aaamegulya@gmail.com>
2022-09-22 16:19:27 +0500

    dont have main and logger

--- homework.py
+++ homework.py
@@ -1,11 +1,19 @@
-...
+import os
+from dotenv import load_dotenv
+from telegram import Bot
+import requests
+import time
+import logging
+import sys
+from logging import StreamHandler
+
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -19,43 +27,39 @@ HOMEWORK_STATUSES = {
 }
 
 
-def send_message(bot, message):
-    ...
+def send_message(bot, message) -> str:
+    """Send message from bot to student."""
+    bot.send_message(message)
 
-
-def get_api_answer(current_timestamp):
+def get_api_answer(current_timestamp=None) -> dict:
+    """Get answer from API Praktikum."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
+    response = requests.get(ENDPOINT, headers=HEADERS, params=params).json()
+    if response:
+        return response
 
-    ...
+def check_response(response) -> dict:
+    """Answer API is correctly?"""
+    return response.get('homeworks') if response.get("homeworks") else None
 
-
-def check_response(response):
-
-    ...
-
-
-def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
+def parse_status(homework) -> str:
+    """Take info from particular homeworks."""
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    verdict = HOMEWORK_STATUSES.get('homework_status')
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
-
-def check_tokens():
-    ...
-
+def check_tokens() -> bool:
+    """Checking tokens for availability."""
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 def main():
-    """Основная логика работы бота."""
-
-    ...
+    """Main logic bot's work."""
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    handler = StreamHandler(stream=sys.stdout)
+    logger.addHandler(handler)
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
@@ -64,7 +68,7 @@ def main():
 
     while True:
         try:
-            response = ...
+            response = get_api_answer()
 
             ...
 
@@ -73,7 +77,7 @@ def main():
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            logging.error(message)
             time.sleep(RETRY_TIME)
         else:
             ...
@@ -81,3 +85,35 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+# def main():
+#     """Main logic bot's work."""
+
+#     ...
+
+#     bot = telegram.Bot(token=TELEGRAM_TOKEN)
+#     current_timestamp = int(time.time())
+
+#     ...
+
+#     while True:
+#         try:
+#             response = ...
+
+#             ...
+
+#             current_timestamp = ...
+#             time.sleep(RETRY_TIME)
+
+#         except Exception as error:
+#             message = f'Сбой в работе программы: {error}'
+#             ...
+#             time.sleep(RETRY_TIME)
+#         else:
+#             ...
+
+
+# if __name__ == '__main__':
+#     main()
