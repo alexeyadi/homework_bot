7d2d2a523460bdca57c0f927e815e3f8be57a45d
Алексей <aaamegulya@gmail.com>
2022-10-05 23:11:37 +0500

    the first iteration

--- homework.py
+++ homework.py
@@ -8,6 +8,15 @@ import sys
 from logging import StreamHandler
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+handler = StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+
 load_dotenv()
 
 
@@ -29,91 +38,69 @@ HOMEWORK_STATUSES = {
 
 def send_message(bot, message) -> str:
     """Send message from bot to student."""
-    bot.send_message(message)
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logger.info('Удачная отправка сообщения в Telegram')
+    except Exception as error:
+        logger.error(f'Сбой при отправке сообщения в Telegram {error}')
 
-def get_api_answer(current_timestamp=None) -> dict:
+
+def get_api_answer(current_timestamp=1) -> dict:
     """Get answer from API Praktikum."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     response = requests.get(ENDPOINT, headers=HEADERS, params=params).json()
     if response:
+        logger.info('Получен доступ к API')
         return response
+    else:
+        logger.error('API Яндекса не доступно')
 
-def check_response(response) -> dict:
+
+def check_response(response) -> list:
     """Answer API is correctly?"""
-    return response.get('homeworks') if response.get("homeworks") else None
+    if response.get('homeworks'):
+        logger.info('Получен корректный ответ от API')
+        return response.get('homeworks')
+    else:
+        logger.error('Некорректный API')
+        return None
+
 
 def parse_status(homework) -> str:
     """Take info from particular homeworks."""
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
-    verdict = HOMEWORK_STATUSES.get('homework_status')
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    if not homework_name:
+        raise KeyError(f'Проблема с ключем: {homework_name}')
+    verdict = HOMEWORK_STATUSES.get(homework_status)
+    return verdict
+
 
 def check_tokens() -> bool:
     """Checking tokens for availability."""
     return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
+
 def main():
     """Main logic bot's work."""
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    handler = StreamHandler(stream=sys.stdout)
-    logger.addHandler(handler)
-
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-
-    ...
-
+    if not check_tokens():
+        logger.critical('Получены не все токены')
+        sys.exit('Проблема с константами!')
+    bot = Bot(token=TELEGRAM_TOKEN)
     while True:
         try:
             response = get_api_answer()
-
-            ...
-
-            current_timestamp = ...
+            homeworks_list = check_response(response)
+            last_homework = homeworks_list[0]
+            status = parse_status(last_homework)
+            send_message(bot, status)
             time.sleep(RETRY_TIME)
-
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             logging.error(message)
             time.sleep(RETRY_TIME)
-        else:
-            ...
 
 
 if __name__ == '__main__':
     main()
-
-
-
-# def main():
-#     """Main logic bot's work."""
-
-#     ...
-
-#     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-#     current_timestamp = int(time.time())
-
-#     ...
-
-#     while True:
-#         try:
-#             response = ...
-
-#             ...
-
-#             current_timestamp = ...
-#             time.sleep(RETRY_TIME)
-
-#         except Exception as error:
-#             message = f'Сбой в работе программы: {error}'
-#             ...
-#             time.sleep(RETRY_TIME)
-#         else:
-#             ...
-
-
-# if __name__ == '__main__':
-#     main()
